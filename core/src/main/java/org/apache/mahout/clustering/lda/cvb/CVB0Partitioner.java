b4ac8aef6247389b7675cd91fbb82fb93d10fed9
Jake Mannix <jake@twitter.com>
2011-08-10 19:05:37 -0700

    partitioner.

--- core/src/main/java/org/apache/mahout/clustering/lda/cvb/CVB0Partitioner.java
+++ core/src/main/java/org/apache/mahout/clustering/lda/cvb/CVB0Partitioner.java
@@ -4,6 +4,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class CVB0Partitioner extends Partitioner<CVBKey, CVBTuple> {
   @Override public int getPartition(CVBKey key, CVBTuple value, int numPartitions) {
-    return ((key.getDocId() + 2) * (key.getTermId() + 2) * 137) % numPartitions;
+    return (((key.getDocId() + 2) * (key.getTermId() + 2) * 137) & Integer.MAX_VALUE) % numPartitions;
   }
 }
